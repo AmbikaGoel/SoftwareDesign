2b0ece8cf8e6145bef45f1863bc5709de94b817c
Ambika Goel <Ambika.Goel@students.olin.edu>
2013-09-25 01:54:47 -0400

    Ambika's grid

--- chap03/grid.py
+++ chap03/grid.py
@@ -1,6 +1,14 @@
-"""Solution to an exercise in Think Python.
+"""Solution to an exercise in Think Python."""
 
-Author:
-"""
+"Author:"
+"Ambika Goel"
 
-print '+ - - - -'
+def grid(a,b,c,d,g,l): 
+	e=c+((b+c)*g)+a # prints ' - - - - - +'
+	f=c*(g*2+1)+d	# prints '           |'
+	h='\n'		# starts new line
+	i=a+e*2		# prints'+ - - - - - - + - - - - - - +'
+	j=d+f*2		# prints '|             |             |'
+	k=i+(h+j)*(l)+h # prints line i, then line j (l) times (basically top 1/2 				  of box)
+	print (k)*2+i
+grid('+','-',' ','|',5,4)
